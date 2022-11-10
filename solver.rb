33d94e24884cce5b2c35b22ee0cf39e1d9a0758d
fullstop125 <hassanmoses125@gmail.com>
2022-11-10 20:46:56 +0300

    fix linter

--- solver.rb
+++ solver.rb
@@ -1,29 +1,29 @@
 class Solver
-    def factorial(n)
-        raise ArgumentError, "n must be >= 0" if n < 0
-        (1..n).inject(:*) || 1
+  def factorial(n)
+    raise ArgumentError, 'n must be >= 0' if n.negative?
 
-    end
+    (1..n).inject(:*) || 1
+  end
 
-    def reverse(word)
-        word.reverse
-    end
+  def reverse(word)
+    word.reverse
+  end
 
-    def fizz_buzz(max)
-        (1..max).map do |n|
-            if n % 15 == 0
-                "FizzBuzz"
-            elsif n % 3 == 0
-                "Fizz"
-            elsif n % 5 == 0
-                "Buzz"
-            else
-                n
-            end
-        end
+  def fizz_buzz(max)
+    (1..max).map do |n|
+      if (n % 15).zero?
+        'FizzBuzz'
+      elsif (n % 3).zero?
+        'Fizz'
+      elsif (n % 5).zero?
+        'Buzz'
+      else
+        n
+      end
     end
+  end
 end
 
 puts Solver.new.factorial(5)
-puts Solver.new.reverse("hello")
+puts Solver.new.reverse('hello')
 puts Solver.new.fizz_buzz(15)
